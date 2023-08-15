0faed8fb986762df91494b7fa9bfaf5d4da21281
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2023-08-15 21:57:33 +0200

    feat: added pie chart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import sqlite3
+import matplotlib.pyplot as plt
 
 conn = sqlite3.connect('budget.db')
 cursor = conn.cursor()
@@ -13,7 +14,7 @@ categories = ["food", "flat"]
 col1, col2 = st.columns(2)
 
 with col1:
-    amount =st.number_input("How much did you spend?", step=0.1)
+    amount = st.number_input("How much did you spend?", step=0.1)
     shop = st.text_input("Name of the shop:")
 
 with col2:
@@ -31,16 +32,35 @@ cursor.execute('''
     )
 ''')
 
+def display_pie_chart(amounts):
+    fig1, ax1 = plt.subplots()
+    ax1.pie(amounts, labels=categories, startangle=90)
+    st.pyplot(fig1)
+
 
 if st.button('Add'):
     cursor.execute('INSERT INTO expenses (category, amount, shop, date) VALUES (?, ?, ?, ?)', (category, amount, shop, date))
     conn.commit()
     st.success('Expense added successfully!')
 
+    # Display expenses
+    st.header('Expenses this month')
+    cursor.execute('''SELECT * 
+                    FROM expenses 
+                    WHERE category = 'food' ''')
+    expenses_food = cursor.fetchall()
 
-# Display expenses
-st.header('Expenses this month')
-cursor.execute('SELECT * FROM expenses')
-expenses = cursor.fetchall()
-for expense in expenses:
-    st.write(f"category: {expense[0]}, amount: {expense[1]}, shop: {expense[2]}, date: {expense[3]}")
+    cursor.execute('''SELECT * 
+                    FROM expenses 
+                    WHERE category = 'flat' ''')
+    expenses_flat = cursor.fetchall()
+
+    sum_food = sum(expense[2] for expense in expenses_food)
+    sum_flat = sum(expense[2] for expense in expenses_flat)
+    amounts = [sum_food, sum_flat]
+
+    display_pie_chart(amounts)
+
+
+
+
